8bc4afea73be51fcf7dd47879008349cfa22e28f
Ryan Stone <ryan@ryanstone.io>
2018-01-16 09:23:58 -0500

    Added cocoapods and logging framework

---
+++ Pods/Target Support Files/Pods-AdfedARTests/Pods-AdfedARTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AdfedARTests : NSObject
+@end
+@implementation PodsDummy_Pods_AdfedARTests
+@end
---
+++ Pods/Target Support Files/Pods-AdfedARUITests/Pods-AdfedARUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AdfedARUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AdfedARUITests
+@end
