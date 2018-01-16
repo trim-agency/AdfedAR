8c5c07a3546ebd28e47930583ad2fdb08cde8389
Ryan Stone <ryan@ryanstone.io>
2018-01-16 10:21:15 -0500

    Remove cocoapods

--- Pods/Target Support Files/Pods-AdfedAR/Pods-AdfedAR-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AdfedAR : NSObject
-@end
-@implementation PodsDummy_Pods_AdfedAR
-@end
--- Pods/Target Support Files/Pods-AdfedARUITests/Pods-AdfedARUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AdfedARUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AdfedARUITests
-@end
