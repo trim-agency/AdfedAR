9bf2eaf05c5330e6176f212a24a3ea12702355e2
Ryan Stone <ryan@ryanstone.io>
2018-02-08 14:53:13 -0500

    Update to instructions label

--- AdfedAR/Enums/UserInstructionsEnum.swift
+++ AdfedAR/Enums/UserInstructionsEnum.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum UserInstructions: String {
-    case none = ""
-    case lookingForSymbol = "Put the page symbol in the circle"
-    case lookingForRectangle = "Pull back and bring the full book into the frame"
-    case lookingForPlane = "Bring the whole book into the frame and try moving to the side or repositioning your phone."
+    case none                   = ""
+    case lookingForSymbol       = "Center the run in the circle"
+    case lookingForRectangle    = "Now pull back and pan to the left or right"
+    case lookingForPlane        = "Bring the whole book into the frame and try moving to the side or repositioning your phone."
 }
