76996750cf1b221551d149e7a09bce67bf0f9724
Ryan Stone <ryan@ryanstone.io>
2018-02-08 15:17:30 -0500

    run -> rune

--- AdfedAR/Enums/UserInstructionsEnum.swift
+++ AdfedAR/Enums/UserInstructionsEnum.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum UserInstructions: String {
     case none                   = ""
-    case lookingForSymbol       = "Center the run in the circle"
+    case lookingForSymbol       = "Center the rune in the circle"
     case lookingForRectangle    = "Now pull back and pan to the left or right"
     case lookingForPlane        = "Bring the whole book into the frame and try moving to the side or repositioning your phone."
 }
